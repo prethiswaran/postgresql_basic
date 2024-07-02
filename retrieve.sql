7b8709430d7a2788bff4f56c4cc24686b7f65b8e
prethiswaran <prethiswaran66@gmail.com>
2024-07-02 16:52:15 +0530

    Add files via upload

---
+++ retrieve.sql
@@ -0,0 +1,6 @@
+select * from cars 
+select car,cylinders,origin from cars
+select distinct(cylinders) from cars
+select distinct(origin)from cars
+select distinct(car)from cars
+select * from cars limit 10
